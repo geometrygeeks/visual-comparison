76ec7bbc30918a450fbec348bc1e093c1a3d8c83
David Toy <d@vidtoy.co.uk>
2017-11-20 14:43:03 +0000

    Missed a file: Added default geometries for comparison

---
+++ src/default-input-bikes.js
@@ -0,0 +1,85 @@
+export default [
+   {
+       'name': 'Giant Defy 2015 L',
+       'reach': 390,
+       'stack': 604,
+       'top_tube': 575,
+       'seat_tube_length': 525,
+       'head_angle': 72.5,
+       'seat_angle': 73,
+       'head_tube': 205,
+       'chainstay': 420,
+       'wheelbase': 1022,
+       'standover': 813,
+       'bb_drop': 50,  // positive bb drop is earthwards
+       'rake': 48,
+       'trail': 61,
+       'wheel_size': 622, // 622 is 700c
+       'tire_width': 20,
+       'head_tube_top_cap': 10,
+       'head_tube_spacer': 15,
+       'stem_height': 20,
+       'stem_angle': -6,
+       'stem_length': 110,
+       'crank': 175,
+   },
+   {
+       'name': 'Specialized Allez Sprint',
+       'reach': 395,
+       'stack': 554,
+       'top_tube': 554,
+       'seat_tube_length': 520,
+       'head_angle': 73.5,
+       'seat_angle': 74,
+       'head_tube': 150,
+       'chainstay': 405,
+       'wheelbase': 984,
+       'standover': 589,
+       'bb_drop': 69,  // positive bb drop is earthwards
+       'bb_height': 272,  // positive bb drop is earthwards
+       'rake': 44,
+       'trail': 55,
+       'stem_length': 100,
+       'crank': 172,
+
+       'fork_length': 368,
+       'seatpost_length': 330,
+
+       'wheel_size': 622, // 622 is 700c
+       'tire_width': 20,
+       'head_tube_top_cap': 10,
+       'head_tube_spacer': 15,
+       'stem_height': 20,
+       'stem_angle': -6,
+
+   },
+   {
+       'name': 'Weird Bike',
+       'reach': 295,
+       'stack': 594,
+       'top_tube': 554,
+       'seat_tube_length': 520,
+       'head_angle': 73.5,
+       'seat_angle': 75,
+       'head_tube': 150,
+       'chainstay': 405,
+       'wheelbase': 984,
+       'standover': 589,
+       'bb_drop': 30,  // positive bb drop is earthwards
+       'rake': 44,
+       'trail': 55,
+       'stem_length': 100,
+       'crank': 172,
+
+       'fork_length': 368,
+       'seatpost_length': 330,
+
+       'wheel_size': 622, // 622 is 700c
+       'tire_width': 20,
+       'head_tube_top_cap': 10,
+       'head_tube_spacer': 15,
+       'stem_height': 20,
+       'stem_angle': -6,
+
+   }
+];
